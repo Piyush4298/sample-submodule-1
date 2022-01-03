78a3985d70effc8218fb340ebc88d033c03c6828
Piyush4298 <piyushpande0405@gmail.com>
2022-01-03 23:52:34 +0530

    second change

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-http = require('http')
-fs = require('fs')
+var fs = require('fs')
 
+var dataFromModule2 = fs.readFileSync('./sample-submodule-2/body.txt', 'utf-8');
+var dataFromModule3 = fs.readFileSync('./sample-submodule-2/sample-submodule-3/intro.txt', 'utf-8');
+
+fs.appendFileSync('newfile.txt', dataFromModule3, function(err){
+    if(err){
+        throw new Error(err);
+    }else{
+        console.log('Module 3 appended successfully')
+    }
+});
+
+fs.appendFileSync('newfile.txt', dataFromModule2, function(err){
+    if(err){
+        throw new Error(err);
+    }else{
+        console.log('Module 2 appended successfully')
+    }
+});
